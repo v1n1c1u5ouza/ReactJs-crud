a48d0bb410de8c08209a6cbbb4e603264be9599d
v1n1c1us5ouza <viniciusdesouzamarinho.com>
2022-11-30 12:07:46 -0300

    Add: Logo to logo componet

--- frontend/src/components/templates/Logo.jsx
+++ frontend/src/components/templates/Logo.jsx
@@ -1,4 +1,11 @@
 import "./Logo.css";
+import logo from "../../assets/img/logo.png";
 import React from "react";
 
-export default (props) => <aside className="logo">Logo</aside>;
+export default (props) => (
+  <aside className="logo">
+    <a href="/" className="logo">
+      <img src={logo} alt="logo" />
+    </a>
+  </aside>
+);
