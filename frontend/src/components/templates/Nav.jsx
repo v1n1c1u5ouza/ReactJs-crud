7b85cc74c7c5553c5fa0fd2ea54f2071fc52c299
v1n1c1us5ouza <viniciusdesouzamarinho.com>
2022-12-01 11:28:42 -0300

    Feat: add menu css

--- frontend/src/components/templates/Nav.jsx
+++ frontend/src/components/templates/Nav.jsx
@@ -1,4 +1,15 @@
 import "./Nav.css";
 import React from "react";
 
-export default (props) => <aside className="meni-area">Navegação</aside>;
+export default (props) => (
+  <aside className="menu-area">
+    <nav className="menu">
+      <a href="#/">
+        <i className="fa fa-home"></i> Início
+      </a>
+      <a href="#/users">
+        <i className="fa fa-users"></i> Usuários
+      </a>
+    </nav>
+  </aside>
+);
