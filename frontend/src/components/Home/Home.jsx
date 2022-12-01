81473a56cf0ff240a6ec86cb9f929604d37474e6
v1n1c1us5ouza <viniciusdesouzamarinho.com>
2022-12-01 11:48:37 -0300

    Feat: Main content to app

---
+++ frontend/src/components/Home/Home.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Main from "../templates/Main";
+
+export default (props) => (
+  <Main
+    icon="home"
+    title="Início"
+    subtitle="Segundo Projeto  do capitulo de React"
+  >
+    <div className="display-4">Bem Vindo!</div>
+    <hr />
+    <p className="mb-0">
+      Sistema para exemplificar a construção de um cadastro desenvolvido em
+      React
+    </p>
+  </Main>
+);
